9c84585391e6e51800490727886fedcba213584f
Arshad Shaik <evercoolarshad@gmail.com>
2015-11-06 19:01:30 -0600

    added print_r() for debugging

--- result.php
+++ result.php
@@ -65,6 +65,7 @@ $result = $rds->describeDBInstances([
    # 'MaxRecords' => <integer>,
 ]);
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+print_r($endpoint);
     print "============\n". $endpoint . "================";
 //echo "begin database";
 $link = mysqli_connect($endpoint,"controller","letmein888","customerrecords") or die("Error " . mysqli_error($link));
