d75e7bfb3510a245c960dd944694c90dc3dbad1b
Arshad Shaik <evercoolarshad@gmail.com>
2015-11-07 01:59:56 -0600

    setup.php trial 1

--- setup.php
+++ setup.php
@@ -1,84 +1,41 @@
+#!/usr/bin/php
 <?php
-// Start the session^M
+// Start the session
 require 'vendor/autoload.php';
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
     'region'  => 'us-east-1'
 ]);
-
-$result = $rds->createDBInstance([
-    'AllocatedStorage' => 10,
-    #'AutoMinorVersionUpgrade' => true || false,
-    #'AvailabilityZone' => '<string>',
-    #'BackupRetentionPeriod' => <integer>,
-   # 'CharacterSetName' => '<string>',
-   # 'CopyTagsToSnapshot' => true || false,
-   # 'DBClusterIdentifier' => '<string>',
-    'DBInstanceClass' => 'db.t1.micro', // REQUIRED
-    'DBInstanceIdentifier' => 'mp1-jrh', // REQUIRED
-    'DBName' => 'customerrecords',
-    #'DBParameterGroupName' => '<string>',
-    #'DBSecurityGroups' => ['<string>', ...],
-    #'DBSubnetGroupName' => '<string>',
-    'Engine' => 'MySQL', // REQUIRED
-    'EngineVersion' => '5.5.41',
-    #'Iops' => <integer>,
-    #'KmsKeyId' => '<string>',
-   # 'LicenseModel' => '<string>',
-  'MasterUserPassword' => 'letmein888',
-    'MasterUsername' => 'controller',
-    #'MultiAZ' => true || false,
-    #'OptionGroupName' => '<string>',
-    #'Port' => <integer>,
-    #'PreferredBackupWindow' => '<string>',
-    #'PreferredMaintenanceWindow' => '<string>',
-    'PubliclyAccessible' => true,
-    #'StorageEncrypted' => true || false,
-    #'StorageType' => '<string>',
-   # 'Tags' => [
-   #     [
-   #         'Key' => '<string>',
-   #         'Value' => '<string>',
-   #     ],
-        // ...
-   # ],
-    #'TdeCredentialArn' => '<string>',
-    #'TdeCredentialPassword' => '<string>',
-   # 'VpcSecurityGroupIds' => ['<string>', ...],
+$s3 = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
 ]);
-
-print "Create RDS DB results: \n";
-# print_r($rds);
-
-$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'mp1-jrh',
-]);
-
-
 // Create a table 
 $result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'mp1-jrh',
+    'DBInstanceIdentifier' => 'mp1-db',
 ]);
-
-
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "============\n". $endpoint . "================\n";
-
-
-
-$link = mysqli_connect($endpoint,"controller","letmein888","3306") or die("Error " . mysqli_error($link)); 
-
-echo "Here is the result: " . $link;
-
-
-$sql = "CREATE TABLE comments 
+$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords") or die("Error " . mysqli_error($link)); 
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+#print "Here is the result: " . $link;
+#global $con;
+$sql = "CREATE TABLE jgldata
 (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
-Title VARCHAR(32),
-Content VARCHAR(500)
+uname VARCHAR(20),
+email VARCHAR(20),
+phone VARCHAR(20),
+filename VARCHAR(256),
+s3rawurl VARCHAR(256),
+s3finishedurl VARCHAR(256),
+state TINYINT(3)CHECK(state IN(0,1,2)),
+date TIMESTAMP 
 )";
-
-$con->query($sql);
-
+$link->query($sql);
+chmod("setup-lite.php", 600);
 ?>
-
